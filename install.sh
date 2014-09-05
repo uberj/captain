844f6990b9c6e0945fdf2e7cf7a566931a95f0e1
uberj <jaxbr503@gmail.com>
2014-09-05 11:53:05 -0700

    add pushd logic

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ if [ $? -eq 1 ]; then
     echo '. /etc/environment' >> /etc/apache2/envvars
 fi
 
-source /etc/environment
-
-cp captain/settings/local.py-heat captain/settings/local.py
-echo 'yes' | python manage.py collectstatic
-echo 'no' | python manage.py syncdb --migrate
+SCRIPT_DIR=$(dirname $0)
+pushd $SCRIPT_DIR
+    cp captain/settings/local.py-heat captain/settings/local.py
+    source /etc/environment
+    echo 'yes' | python manage.py collectstatic
+    echo 'no' | python manage.py syncdb --migrate
+popd
