e8b5a276182efd86675f21fc21529d29bb0a6d73
uberj <jaxbr503@gmail.com>
2014-09-25 14:54:29 -0700

    gah 2

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ pushd $SCRIPT_DIR
     cp captain/settings/local.py-heat captain/settings/local.py
     source /etc/environment
     echo 'yes' | python manage.py collectstatic
-    #echo 'CREATE TABLE create_lock(l int);' | python manage.py dbshell
-    #if [ $? -eq 0 ]; then
-    #    echo "Creating tables"
-    #    echo 'no' | python manage.py syncdb --migrate
-    #fi
-    #echo "Tables already created"
+    echo 'CREATE TABLE create_lock(l int);' | python manage.py dbshell
+    if [ $? -eq 0 ]; then
+        echo "Creating tables" > /tmp/foo
+        echo 'no' | python manage.py syncdb --migrate
+    fi
+    echo "Tables already created"
 popd
 
 service apache2 restart
